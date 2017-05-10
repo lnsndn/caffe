01328482f20ea8225b6d9f28106a064d48ceebb2
Sebastian Sudholt <sebastian.sudholt@tu-dortmund.de>
2017-05-10 09:42:48 +0200

    multiple changes
    * running SSH check every time cmake is called
    * added prometheus to the list of GPU machines
    
    Resolved Conflicts:
    	cmake/Cuda.cmake
    	patrec_gpu_machines.txt

--- cmake/Cuda.cmake
+++ cmake/Cuda.cmake
@@ -49,6 +49,60 @@ function(caffe_detect_installed_gpus out_variable)
   endif()
 endfunction()
 
+################################################################################################
+# A function for automatic detection of all GPUs installed in the PatRec machines
+# Usage:
+#   caffe_detect_installed_gpus(out_variable)
+function(caffe_detect_patrec_gpus out_variable)
+  #if(NOT CUDA_gpu_detect_output)
+    set(__cufile ${PROJECT_BINARY_DIR}/detect_patrec_cuda_archs.cu)
+
+    message(STATUS "Detecting GPUs for all PatRec machines...")
+    file(WRITE ${__cufile} ""
+      "#include <cstdio>\n"
+      "int main()\n"
+      "{\n"
+      "  int count = 0;\n"
+      "  if (cudaSuccess != cudaGetDeviceCount(&count)) return -1;\n"
+      "  if (count == 0) return -1;\n"
+      "  for (int device = 0; device < count; ++device)\n"
+      "  {\n"
+      "    cudaDeviceProp prop;\n"
+      "    if (cudaSuccess == cudaGetDeviceProperties(&prop, device))\n"
+      "      std::printf(\"%d.%d \", prop.major, prop.minor);\n"
+      "  }\n"
+      "  return 0;\n"
+      "}\n")
+    
+    file(STRINGS ${PROJECT_SOURCE_DIR}/patrec_gpu_machines.txt __patrec_gpu_machines)
+    message(STATUS "Found following GPU machines: ${__patrec_gpu_machines}")
+    set(__nvcc_out "")
+    set(__nvcc_res 0)
+    foreach(__machine ${__patrec_gpu_machines})
+      execute_process(COMMAND ssh "${__machine}" "${CUDA_NVCC_EXECUTABLE}" "--run" "-Wno-deprecated-gpu-targets" "${__cufile}"
+                      RESULT_VARIABLE __nvcc_res_machine
+                      OUTPUT_VARIABLE __nvcc_out_machine)
+      message(STATUS "GPU architectures on ${__machine}: ${__nvcc_out_machine}")
+      if(__nvcc_res_machine EQUAL 0)
+        set(__nvcc_out "${__nvcc_out} ${__nvcc_out_machine}")
+      else()
+        set(_nvcc_res 1)
+      endif()
+    endforeach(__machine)
+
+    if(__nvcc_res EQUAL 0)
+      string(REPLACE "2.1" "2.1(2.0)" __nvcc_out "${__nvcc_out}")
+      set(CUDA_gpu_detect_output ${__nvcc_out} CACHE INTERNAL "Returned GPU architetures from caffe_detect_gpus tool" FORCE)
+    endif()
+  #endif()
+
+  if(NOT CUDA_gpu_detect_output)
+    message(STATUS "Automatic GPU detection failed. Building for all known architectures.")
+    set(${out_variable} ${Caffe_known_gpu_archs} PARENT_SCOPE)
+  else()
+    set(${out_variable} ${CUDA_gpu_detect_output} PARENT_SCOPE)
+  endif()
+endfunction()
 
 ################################################################################################
 # Function for selecting GPU arch flags for nvcc based on CUDA_ARCH_NAME
